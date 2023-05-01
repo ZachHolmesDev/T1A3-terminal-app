aadaf4c564b2ff3daff13bf31563eef9b8fb74db
zach <zholmes430@gmail.com>
2023-05-01 12:11:48 +1000

    added more info to display current weather

--- hold.py
+++ hold.py
@@ -1,12 +1,6 @@
-print(response.json()['location']['name'])
-
 #  tomorrow.io API
-url = "https://api.tomorrow.io/v4/weather/forecast?location=newyork&apikey=1xKR2c5vCp2WQO6ci3o6FljhPuTkB2GP"
-headers = {"accept": "application/json"}
-response = requests.get(url, headers=headers)
-print(response.text)
-
-
+def get_current_weather_tmrio(selected_city):
+    selected_city = requests.get(f"https://api.tomorrow.io/v4/weather/forecast?location={selected_city}&apikey=1xKR2c5vCp2WQO6ci3o6FljhPuTkB2GP")
 # OWM 
 "api.openweathermap.org/data/2.5/weather?id=524901&appid=YOUR_API_KEY"
 api_key_owm = "814573ba0a3f07c527d7b784fee3eff0"
--- tst.py
+++ tst.py
@@ -16,7 +16,7 @@ def print_menu(selected_city,):
     print("---Welcome to the the dvd surround sound test---")
     print("[s] Select City ")
     print("[1] Get current weather for Selected City ")
-    print("[2] **Display forecast")
+    print("[2] Display forecast for Selected City")
     print("[3] **Get Historical Data")
     print("[4] **Export something???")
     print("[q] Quit")
@@ -25,40 +25,73 @@ def print_menu(selected_city,):
 
     
 def print_current_weather(current_weather_response):
-    print("---",current_weather_response.json()['location']['name'],"--------------------------")
-    print(current_weather_response.json()['current']['condition']['text'])
-    print('temp_c =' , current_weather_response.json()['current']['temp_c'])
-    print('feelslike_c =' , current_weather_response.json()['current']['feelslike_c'])
+    print("Current weather for: ",current_weather_response.json()['location']['name'],)
+    print('last_updated          =', current_weather_response.json()['current']['last_updated'])
+    print('localtime             =', current_weather_response.json()['location']['localtime'])
+    print('current condition     =',current_weather_response.json()['current']['condition']['text'])
+    print('')
+    print('Temps')
+    print('temp c                =' , current_weather_response.json()['current']['temp_c'])
+    print('feelslike c           =' , current_weather_response.json()['current']['feelslike_c'])
+    print('')
+    print('Precipitation')
+    print('precip mm             =' , current_weather_response.json()['current']['precip_mm'])
+    print('humidity              =' , current_weather_response.json()['current']['humidity'])
+    print('cloud                 =' , current_weather_response.json()['current']['cloud'])
+    print('pressure_mb           =' , current_weather_response.json()['current']['pressure_mb'])
+    print('')
+    print('Visability')
+    print('vis km                =' , current_weather_response.json()['current']['vis_km'])
+    print('')
+    print('Wind')
+    print('wind kph              =' , current_weather_response.json()['current']['wind_kph'])
+    print('wind dir              =' , current_weather_response.json()['current']['wind_dir'])
+    print('wind degree           =' , current_weather_response.json()['current']['wind_degree'])
+    print('gust kph              =' , current_weather_response.json()['current']['gust_kph'])
+    # print('' , current_weather_response.json()['current'][''])
+    # print('' , current_weather_response.json()['current'][''])
+    # print('' , current_weather_response.json()['current'][''])
     print("-----------------------------------------------")
     
     
 # menu functions
 # --------------------------
 def menu():
-    selected_city = None
+    selected_city = "Brisbane"
     current_weather_response = None
+    forecast_response = None 
+
     # info = None
     print_ui(selected_city, current_weather_response)
     
     if selected_city == None:
         print("No City selected")
         selected_city = input("Give City Name: ")
+        print("")
+        print("")
     if selected_city == "q":
          return
      
     while True:
         print_ui(selected_city, current_weather_response)
         num = input("Enter otion number here: ")
+        print("")
+        print("")
         match num:
             case "s":
                 selected_city = input("Give New City Name: ")
                 if selected_city == "q":
                     break
-                current_weather_response = None
+                current_weather_response = None 
+                forecast_response = None 
                 continue
             case '1':
                 current_weather_response = get_current_weather_wapi(selected_city)
                 write_response(current_weather_response)
+                continue
+            case '2':
+                forecast_response = get_forecast_wapi(selected_city)
+                write_response(forecast_response)
                 continue
             case "q":
                 break
@@ -70,6 +103,9 @@ def get_current_weather_wapi(selected_city):
     current_weather_response = requests.get(f"http://api.weatherapi.com/v1/current.json?q={selected_city}&key={api_key_wapi_caleb}")
     return current_weather_response
 
+def get_forecast_wapi(selected_city):
+    forecast_response = requests.get(f"http://api.weatherapi.com/v1/forecast.json?q={selected_city}&key={api_key_wapi_caleb}")
+    return forecast_response
 
 # def get_current_weather_tmrio(selected_city):
 #     selected_city = requests.get(f"https://api.tomorrow.io/v4/weather/forecast?location={selected_city}&apikey=1xKR2c5vCp2WQO6ci3o6FljhPuTkB2GP")
