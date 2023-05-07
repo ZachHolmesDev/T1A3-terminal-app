a87019e3d22cea22261686b85813f7af17e9f603
zach <zholmes430@gmail.com>
2023-05-07 16:19:40 +1000

    added error handling for if the api is unreachable

--- main.py
+++ main.py
@@ -7,39 +7,43 @@ api_key_wapi_caleb = "3328658fef7c4737a1635629232204"
 
 # Ui functions
 # --------------------------
-"""
-This is a set of functions that prints different parts of a weather CLI interface in Python.
-
-:param selected_city: A string representing the currently selected city for weather information
-:param current_weather_response: It is a response object obtained from an API call to retrieve the
-current weather data for a selected city. It contains information such as the location name, current
-condition, temperature, precipitation, visibility, wind speed and direction, etc
-:param forecast_response: The response object containing the forecast data for a selected city
-:param history_response: The response object containing the weather history data for a selected city
-:param menu_option: The selected option from the main menu, which determines which information to
-display or which export option to choose
-"""
 def print_ui(selected_city, current_weather_response, forecast_response, history_response, menu_option):
+    """
+    The function `print_ui` takes in various API responses and a menu option, and prints out the
+    corresponding weather information or error message based on the selected menu option.
+    
+    :param selected_city: 
+        The name of the city that the user has selected
+    :param current_weather_response: 
+        The response object containing the current weather data for aselected city
+    :param forecast_response: 
+        The forecast_response parameter is a response object returned by an API
+        call that contains the forecast weather data for a selected city. It is used in the print_forecast
+        function to display the forecasted weather information for the selected city
+    :param history_response: 
+        The response object containing the weather history data for a selected city
+    :param menu_option: 
+        A string representing the user's selected option from the main menu
+    """
     # clears the commandline to keep ui clean
     os.system('cls' if os.name == 'nt' else 'clear')
         
     print_menu(selected_city)
-    if menu_option == '1':
+    if menu_option == '1' and current_weather_response is not None:
         print_current_weather(current_weather_response)
     
-    elif menu_option == '2':
+    elif menu_option == '2' and forecast_response is not None:
         print_forecast(forecast_response)
     
-    elif menu_option == '3':
+    elif menu_option == '3' and history_response is not None:
         print_history(history_response, selected_city)
     
     elif menu_option == '4':
         print_export_options(selected_city)
     
 # error handling for invalid inputs in the main menu 
-# this is handled different in the sub menu so should probbably be made more consistent but it is functional currently 
-    elif menu_option is not None and menu_option != 's': 
-        print("Option input invalid try again")
+    elif menu_option not in {'s', '1', '2', '3', '4', 'q', None}:
+        print(f"menu option '{menu_option}' invalid try again")
     print('')
 
 def print_menu(selected_city,):
@@ -114,24 +118,26 @@ def print_export_options(selected_city):
     print("    for each export you request a new file will be created ")
     print("    in the export folder with the apropriate name and date time stamp")
     print("    [s] Select City ")
-    print("    [1] Export 24 HR forecast                   .JSON")
-    print("    [2] Export Weather History for last 7 days  .JSON")
-    print("    [3] Export Current Weather                  .JSON")
+    print("    [1] Export Current Weather                  .JSON")
+    print("    [2] Export 24 HR forecast                   .JSON")
+    print("    [3] Export Weather History for last 7 days  .JSON")
     print("    [0] Return to main menu")
     print("    [q] Quit Program")
     print(f"Selected City: ", {selected_city})
     
 # main function asks for an inital city then uses a loop to render the ui and call the options for the user untill they exit
-"""
-This is the main function of the application that allows the user to select a city, view
-current weather, forecast, and historical data, and export the data in different formats.
-:return: The function `main()` is not returning anything. It contains a series of statements and
-loops that execute based on user input.
-"""
+    """
+    The main function allows the user to select a city and then presents a menu of options to retrieve
+    and export weather data.
+    
+    :return: 
+        The main function does not return any value. It uses the `return` statement to exit the
+        function and terminate the program when the user selects the "q" option.
+    """
 # --------------------------
 def main():
     # set as brisbane to expidite testing
-    selected_city             = "Brisbane"
+    selected_city             = None
     current_weather_response  = None
     forecast_response         = None 
     menu_option               = None 
@@ -143,9 +149,9 @@ def main():
     if selected_city == None:
         print("")
         print("No City selected")
-        selected_city = input("Give City Name: ")
-    if selected_city == "q":
-        return
+        selected_city = select_new_city()
+        if selected_city == 'q':
+            return
 
     while True:
         print_ui(selected_city, current_weather_response, forecast_response, history_response, menu_option)
@@ -173,7 +179,8 @@ def main():
                 # write_response(history_response)
                 continue
             case '4':
-                # this loop handles the sub menu functionality
+                # this loop handles the sub menu functionality it is ordered differntly then the main loop 
+                # to facilitate the printing of sucess messages for export functions
                 while export_option != '0':
                     success_message = None
 
@@ -183,15 +190,15 @@ def main():
                             selected_city = select_new_city()
                             if selected_city == 'q':
                                 return
-                        # Export forecast
+                        # Export current weather data
                         case '1':
+                            success_message = export_response(selected_city, "current weather")
+                        # Export forecast
+                        case '2':
                             success_message = export_response(selected_city, "forecast")
                         # Export historical data
-                        case '2':
-                            success_message = export_response(selected_city, "history")
-                        # Export current weather data
                         case '3':
-                            success_message = export_response(selected_city, "current weather")
+                            success_message = export_response(selected_city, "history")
                         case 'q':
                             return
 
@@ -201,10 +208,10 @@ def main():
                         print(success_message)
 
                     if export_option not in {'s', '1', '2', '3', 'q', '0', None}:
-                        print('export option invalid try again')
+                        print(f"export option '{export_option}' invalid try again")
 
                     export_option = input("Enter export option number here: ")
-                # resets options to keep UI clean
+                # resets options to keep UI clean and avoid unintended behaviour
                 export_option = None
                 menu_option   = None    
             
@@ -217,101 +224,151 @@ def main():
 These are Python functions that use the WeatherAPI to retrieve current weather, forecast, and
 historical weather data for a selected city.
 
-:param selected_city: The name of the city for which weather data is being requested
-:return: The functions are returning responses from the WeatherAPI for current weather, forecast,
-and historical weather data for a selected city.
+:param selected_city: 
+    The name of the city for which weather data is being requested
+:return: 
+    The `get_current_weather_wapi`, `get_forecast_wapi`, and `get_history_wapi` functions are
+    returning the responses from API requests made to the WeatherAPI service for the current weather,
+    forecast, and historical weather data for a selected city. If there is an error with the request,
+    the functions allert the user and return `None`.
 """
+
 # ------------------------------
 def get_current_weather_wapi(selected_city):
-    current_weather_response = requests.get(f"http://api.weatherapi.com/v1/current.json?q={selected_city}&key={api_key_wapi_caleb}")
+    try:
+        current_weather_response = requests.get(f"http://api.weatherapi.com/v1/current.json?q={selected_city}&key={api_key_wapi_caleb}")
+        current_weather_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print()
+        print(f"Error: {e}")
+        print()
+        input('press enter to continue: ')
+        return None
     return current_weather_response
 
 def get_forecast_wapi(selected_city):
-    forecast_response = requests.get(f"http://api.weatherapi.com/v1/forecast.json?q={selected_city}&key={api_key_wapi_caleb}")
+    try:
+        forecast_response = requests.get(f"http://api.weatherapi.com/v1/forecast.json?q={selected_city}&key={api_key_wapi_caleb}")
+        forecast_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print()
+        print(f"Error: {e}")
+        print()
+        input('press enter to continue: ')
+        return None
     return forecast_response
 
 # Currently get the full 7 day history from todays date 
 def get_history_wapi(selected_city):
-        
-    today  = date.today()
-    # dt is the past date to start the histoy request 
-    dt     = today - timedelta(days=7)
-    # end_dt is a date in the futre from dt
-    end_dt = today - timedelta(days=1)
+    try:
+        today  = date.today()
+        dt     = today - timedelta(days=7)
+        end_dt = today - timedelta(days=1)
     
-    forecast_response = requests.get(f"http://api.weatherapi.com/v1/history.json?q={selected_city}&key={api_key_wapi_caleb}&dt={dt}&end_dt={end_dt}")
+        forecast_response = requests.get(f"http://api.weatherapi.com/v1/history.json?q={selected_city}&key={api_key_wapi_caleb}&dt={dt}&end_dt={end_dt}")
+        forecast_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print()
+        print(f"Error: {e}")
+        print()
+        input('press enter to continue: ')
+        return None
     return forecast_response
 
 # other functions
-    """
-    The function 
-    `select_new_city()` 
-    prompts the user to input a new city name and checks if it is a
-    valid location using the `check_loc_valid()` function.
-    :return: The code snippet contains two functions. The `select_new_city()` function returns the
-    selected city name entered by the user or "q" if the user chooses to quit. 
-    
-    The function
-    `check_loc_valid(selected_city)`  
-    returns a boolean value indicating whether the selected
-    city is valid or not. It returns `True` if the city is valid and `False` if it is not valid.
-    """
+"""
+The function `select_new_city()` 
+    prompts the user to input a new city name and checks if it is valid
+    using the `check_loc_valid()` function.
+:return: 
+    The function `select_new_city()` returns the selected city name entered by the user or "q"
+    if the user chooses to quit. The function `check_loc_valid(new_city)` returns a tuple containing a
+    boolean value indicating whether the city name is valid and a string error message if the city name
+    is invalid.
+"""
 # -------------------------------
 def select_new_city():
     while True:
         print('')
-        new_city      = input("Give New City Name: ")
-        city_is_valid = check_loc_valid(new_city)
-
+        new_city = input("Give New City Name: ")
+        city_is_valid, error_msg = check_loc_valid(new_city)
+        
         if new_city == "q":
             selected_city = new_city
             return selected_city
+
         elif city_is_valid:
             selected_city = new_city
-            return selected_city
-        else:
-            error_text = f"City name '{new_city}' is invalid. Please try again."
-            print(error_text)
+            return selected_city        
                        
-def check_loc_valid(selected_city):
-        loc_check = requests.get(f"http://api.weatherapi.com/v1/current.json?q={selected_city}&key={api_key_wapi_caleb}")
-        try:
-            loc_check.json()['error']
-        except:
-            return True
+import requests.exceptions
 
-        return False
+def check_loc_valid(new_city):
+    try:
+        if new_city == 'q':
+            return True, ""
+        loc_check = requests.get(f"http://api.weatherapi.com/v1/current.json?q={new_city}&key={api_key_wapi_caleb}")
+        loc_check.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        if isinstance(e, requests.exceptions.ConnectionError):
+            error_msg = "Error: Unable to connect to the API. Please check your internet connection."
+        else:
+            error_msg = f"    Error: City name '{new_city}' is invalid."
+        print()
+        print(error_msg)
+        print()
+        input('    Press enter to try again: ')
+        return False, error_msg
+
+    if loc_check is None:
+        error_msg = "Error: API is unavailable."
+        print()
+        print(error_msg)
+        print()
+        input('Press enter to continue: ')
+        return False, error_msg
+
+    return True, ""
+
 
 # exports the data as a .json file
 """
 This function exports weather data for a selected city and call type as a JSON file with a
 timestamped filename.
 
-:param selected_city: The name of the city for which the weather data is being exported
-:param call_type: The call_type parameter is a string that specifies the type of weather data to
-retrieve. It can be one of three values: "forecast", "history", or "current weather"
-:return: a string indicating the successful export of a JSON file containing weather data for a
-selected city and call type. The string includes the file path and name of the exported file.
+:param selected_city: 
+    The name of the city for which the weather data is being fetched and exported
+:param call_type:
+    The type of weather data to retrieve, which can be "forecast", "history", or
+    "current weather"
+:return: 
+    a string indicating whether the export was successful or not, and the path to the exported
+    file. If there was an error fetching the data, the function returns an error message.
 """
 # Note this function dose not include error handling for if the API is down 
 def export_response(selected_city, call_type):
     time_now = datetime.now()
     time_stamp = time_now.strftime('%Y-%m-%d %H:%M:%S')
-    
-    if   call_type == "forecast":
+
+    if call_type == "forecast":
         export = get_forecast_wapi(selected_city)
     elif call_type == "history":
         export = get_history_wapi(selected_city)
     elif call_type == "current weather":
         export = get_current_weather_wapi(selected_city)
-    
+
+    if export is None:
+        return f"Error: Failed to fetch {call_type} data for {selected_city}."
+
     os.makedirs('EXPORTS', exist_ok=True)
-    export = json.dumps(export.json(), indent=5)
-    
+    export_data = json.dumps(export.json(), indent=5)
+
     with open(f'EXPORTS/{selected_city}_{call_type}_export_{time_stamp}.json', 'w') as file:
-        file.write(export)
+        file.write(export_data)
     return f'Successful export: EXPORTS/{selected_city}_{call_type}_export_{time_stamp}.json'
 
+
+# write_response() is a testing function i used during development to write responses to a file so i could see what the api is returning
 """    
 The bellow code defines a function called `write_response` that takes in a `response` object as an
 argument. The function checks if the response status code is 200 and if the response text is not
@@ -319,7 +376,6 @@ empty. If both conditions are met, the response is converted to a JSON string wi
 written to a file called `rqsts.json` in the `EXPORTS` directory. If the response status code is not
 200 or the response text is empty, a `ValueError` is raised. If there is an error decoding the
 response JSON, a `JSONDecodeError` is caught
-testing function used to write responses to a file so i can see what to api is returning
 """
 def write_response(response):
     try:
